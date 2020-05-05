68fc84afa9f7fb48c55aea8ab7221df1cb4c3c48
DevCoderGod <devcodergod@gmail.com>
2020-05-05 21:02:10 +0300

    patch2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-myTopnav.onclick=function burgerClick(){
-	
-	var x=document.getElementById('myTopnav')
+menu.onclick=function burgerClick(){
+	var x=document.getElementById('topnav')
 
 	if(x.className==='header__topnav'){
 		x.className+=' header__topnav-responsive'
 	}
 	else{x.className='header__topnav'}
+}
+topnav.onclick=function navClick(){
+	var x=document.getElementById('topnav')
+
+	if(x.className==='header__topnav header__topnav-responsive'){
+		x.className='header__topnav'
+	}
 }
