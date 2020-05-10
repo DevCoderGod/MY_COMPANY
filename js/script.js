5fabb0dec7bf751a7525b0bd86d2c37dd083407f
DevCoderGod <devcodergod@gmail.com>
2020-05-10 16:42:29 +0300

    V1.03
    Signed-off-by: DevCoderGod <devcodergod@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 menu.onclick=function burgerClick(){
 	var x=document.getElementById('topnav')
-
-	if(x.className==='header__topnav'){
-		x.className+=' header__topnav-responsive'
-	}
-	else{x.className='header__topnav'}
+	x.classList.toggle("header__topnav-responsive")
+	document.body.classList.toggle("lock")
 }
-topnav.onclick=function navClick(){
+ topnav.onclick=function navClick(){
 	var x=document.getElementById('topnav')
+	x.classList.toggle("header__topnav-responsive")
+	document.body.classList.toggle("lock")
+ }
 
-	if(x.className==='header__topnav header__topnav-responsive'){
-		x.className='header__topnav'
-	}
-}
+// 	if(x.className==='header__topnav'){
+// 		x.className+=' header__topnav-responsive'
+// 	}
+// 	else{x.className='header__topnav'}
+// }
+// topnav.onclick=function navClick(){
+// 	var x=document.getElementById('topnav')
+
+// 	if(x.className==='header__topnav header__topnav-responsive'){
+// 		x.className='header__topnav'
+// 	}
+// }
