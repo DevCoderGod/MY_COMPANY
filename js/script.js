724ebc3a24d2d36c480a6afc1bd936434ae908be
DevCoderGod <devcodergod@gmail.com>
2020-05-12 20:26:28 +0300

    исправлено меню

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
-menu.onclick=function burgerClick(){
-	var x=document.getElementById('topnav')
-	x.classList.toggle("header__topnav-responsive")
-	document.body.classList.toggle("lock")
+onclick=function(event){
+	if(event.target===burger){
+		document.getElementById('nav').classList.toggle("header__nav--responsive")
+		document.body.classList.toggle("lock");
 }
- topnav.onclick=function navClick(){
-	var x=document.getElementById('topnav')
-	x.classList.toggle("header__topnav-responsive")
-	document.body.classList.toggle("lock")
- }
-
-// 	if(x.className==='header__topnav'){
-// 		x.className+=' header__topnav-responsive'
-// 	}
-// 	else{x.className='header__topnav'}
-// }
-// topnav.onclick=function navClick(){
-// 	var x=document.getElementById('topnav')
-
-// 	if(x.className==='header__topnav header__topnav-responsive'){
-// 		x.className='header__topnav'
-// 	}
-// }
+	else{
+		document.getElementById('nav').classList.remove("header__nav--responsive")
+		document.body.classList.remove("lock");
+		let link=document.getElementById(event.target.innerHTML);
+		if(link!=null){
+			link.scrollIntoView(true);
+			//далее ниндзя код :-)
+			window.scrollBy(0,(-parseInt(this.getComputedStyle(document.getElementsByClassName("header")[0]).height,10))+2)
+			//
+			//let a=document.getElementsByClassName("header");
+			//let z=getComputedStyle(a[0]).height;
+			//let q=parseInt(z,10);
+			//let s=window.scrollBy(0,-q+2);
+		}
+	}
+}
